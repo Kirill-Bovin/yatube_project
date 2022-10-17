80dbd3a3803736955d2d4ce6e1554d64dfa43fa9
Kirill Bovin <skrill96@e1.ru>
2022-10-17 15:16:12 +0500

    Добавлены страницы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):    
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = 'Главная страница'
+    context = {
+        'title': title,
+    } 
+    return render(request, template, context)
+
 
 def group_posts(request, slug):
-    return HttpResponse('Страница групп') 
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+    } 
+        
+    return render(request, template, context)
