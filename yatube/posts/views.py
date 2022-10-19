f41827e741f4f472eb95379e9a845e6e8d21ab21
Kirill Bovin <skrill96@e1.ru>
2022-10-19 20:16:38 +0500

    Final project

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post
+from .models import Group
 
-# Create your views here.
-def index(request):    
-    template = 'posts/index.html'
-    title = 'Главная страница'
+def index(request):
+    
+    posts = Post.objects.order_by('-pub_date')[:10]
+   
     context = {
-        'title': title,
-    } 
-    return render(request, template, context)
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
+    
+    group = get_object_or_404(Group, slug=slug)
+    
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title,
-    } 
-        
-    return render(request, template, context)
+        'group': group,
+        'posts': posts
+        }
+    return render(request, 'posts/group_list.html', context) 
