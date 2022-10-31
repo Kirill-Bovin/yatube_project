2e861eb4ca0b71f98c10f6d0a83575d7dd736d4a
Kirill Bovin <skrill96@e1.ru>
2022-10-31 21:37:24 +0500

    Saticnue stranicy

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import get_object_or_404, render
+from django.conf import settings
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
 
-from .models import Group, Post
+from .models import Group, Post, User
 
 
 def index(request):
     posts = Post.objects.all()
-    paginator = Paginator(posts, 10)
+    paginator = Paginator(posts, settings.NUMBER_OF_POSTS)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
@@ -18,7 +19,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()
-    paginator = Paginator(posts, 10)
+    paginator = Paginator(posts, settings.NUMBER_OF_POSTS)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
@@ -27,3 +28,35 @@ def group_posts(request, slug):
     }
 
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = author.posts.all()
+    post_count = author.posts.count()
+    paginator = Paginator(posts, settings.NUMBER_OF_POSTS)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'post_count': post_count,
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post_title = post.text[:settings.SLICE_END]
+    author = post.author
+    post_count = author.posts.count()
+    author_posts = post.author.posts.all()
+    context = {
+        'post': post,
+        'post_title': post_title,
+        'author': author,
+        'author_posts': author_posts,
+        'post_count': post_count,
+    }
+    return render(request, 'posts/post_detail.html', context)
